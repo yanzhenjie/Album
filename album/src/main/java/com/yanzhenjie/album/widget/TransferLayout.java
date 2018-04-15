77ede4901902978fc3c5801b79fcd49fa64bb369
YanZhenjie <smallajax@foxmail.com>
2018-04-15 17:55:21 +0800

    TraserLayout transfers click events.

---
+++ album/src/main/java/com/yanzhenjie/album/widget/TransferLayout.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright © 2018 Yan Zhenjie.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.yanzhenjie.album.widget;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Created by YanZhenjie on 2018/4/11.
+ */
+public class TransferLayout extends FrameLayout {
+
+    public TransferLayout(@NonNull Context context) {
+        super(context);
+    }
+
+    public TransferLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs, 0);
+    }
+
+    public TransferLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean performClick() {
+        if (getChildCount() == 1) {
+            return getChildAt(0).performClick();
+        }
+        return super.performClick();
+    }
+
+    @Override
+    public boolean callOnClick() {
+        if (getChildCount() == 1) {
+            return getChildAt(0).performClick();
+        }
+        return super.performClick();
+    }
+}
